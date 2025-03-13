b72ae65560d06375f518c4dab88ccbfed8937838
Minhpt <minhpt.pc19@gmail.com>
2025-03-13 14:56:58 +0700

    update native splash

--- lib/r.dart
+++ lib/r.dart
@@ -1,5 +1,7 @@
 class AssetImages {
 	static const String appIcon = 'assets/images/app_icon.png';
+	static const String branding = 'assets/images/branding.png';
+	static const String splashLogo = 'assets/images/splash_logo.png';
 }
 
 class AssetIcons {
